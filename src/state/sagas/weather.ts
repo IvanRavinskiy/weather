365e9a159845d76ad7a5654474fd32d8029a0702
IvanRavinskiy <ivan.ravinskiy@gmail.com>
2022-10-30 18:01:47 +0300

    fix saga

--- src/state/sagas/weather.ts
+++ src/state/sagas/weather.ts
@@ -4,9 +4,7 @@ import {createAPI} from '../../utils/createAPI';
 import {WEATHER_SUCCESS} from '../reducers/weather';
 
 export function* weather() {
-  console.log('weather saga work');
   const city = yield select(getCity);
   const data = yield call(createAPI, city);
   yield put(WEATHER_SUCCESS(data));
-  console.log('weather saga end');
 }
